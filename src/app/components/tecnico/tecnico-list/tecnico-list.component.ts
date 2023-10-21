2e5091f278c2980c865d2223f233bfc6d4b8f3a6
marwamprog-js <marwamprog.js@gmail.com>
2023-10-21 10:43:05 -0300

    Component LOGIN criado

--- src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
+++ src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Tecnico } from 'src/app/models/tecnico';
 
 @Component({
   selector: 'app-tecnico-list',
@@ -6,5 +9,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./tecnico-list.component.css']
 })
 export class TecnicoListComponent {
+  ELEMENT_DATA: Tecnico[] = [
+    {
+      id: 1,
+      nome: 'Valdir Cezar',
+      cpf: '123.456.789-89',
+      email: 'valdir@email.com',
+      senha: '123456',
+      perfis: ['0'],
+      dataCriacao: '15/08/2022'
+    }
+  ]
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol', 'acoes'];
+  dataSource = new MatTableDataSource<Tecnico>(this.ELEMENT_DATA);
 
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
 }
+
+
+
