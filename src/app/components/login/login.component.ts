a43a2cefc75e6d2a82a7f3de19d2890551c6a55f
marwamprog-js <marwamprog.js@gmail.com>
2023-10-23 10:16:57 -0300

    Validação de formulários para login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { Credenciais } from 'src/app/models/credenciais';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+
+  creds: Credenciais = {
+    email: '',
+    senha: ''
+  }
+
+  email = new FormControl(null, Validators.email);
+  senha = new FormControl(null, Validators.minLength(3));
+
+  validaCampos(): boolean {
+    if(this.email.valid && this.senha.valid) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 
 }
