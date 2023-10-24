3174bbfeab4ee758486339cd29583df95da9a5bd
marwamprog-js <marwamprog.js@gmail.com>
2023-10-24 15:13:12 -0300

    Crud completo para cliente

---
+++ src/app/components/cliente/cliente-delete/cliente-delete.component.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Cliente } from 'src/app/models/cliente';
+import { ClienteService } from 'src/app/services/cliente.service';
+
+@Component({
+  selector: 'app-cliente-delete',
+  templateUrl: './cliente-delete.component.html',
+  styleUrls: ['./cliente-delete.component.css']
+})
+export class ClienteDeleteComponent {
+
+  cliente: Cliente = {
+    id: '',
+    nome: '',
+    cpf: '',
+    email: '',
+    senha: '',
+    perfis: [],
+    dataCriacao: ''
+  }
+  
+  constructor(
+    private service: ClienteService,
+    private toast: ToastrService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+      this.cliente.id = this.route.snapshot.paramMap.get('id');
+      this.findById();
+  }
+
+  findById(): void {
+    this.service.findById(this.cliente.id).subscribe( response => {
+      response.perfis = [];
+      this.cliente = response;
+    });
+  }
+  
+  delete(): void {
+    this.service.delete(this.cliente.id)
+    .subscribe( () => {
+      this.toast.success('Cliente "' + this.cliente.nome + '" deletado com sucesso', 'Update');
+      this.router.navigate(['clientes']);
+    }, ex => {
+      if(ex.error.errors) {
+        ex.error.errors.forEach(element => {
+          this.toast.error(element.message);
+        });
+      } else  {
+        this.toast.error(ex.error.message);
+      }
+    });
+  }
+
+  
+
+}
